ee616a07a262ced4e7385ce7e3083666485ff3e6
Andy Kirkham <andy@spiders-lair.com>
2015-12-10 11:13:31 +0000

    Use ENV to disable VCS

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ ln -s /src $OPENGROK_INSTANCE_BASE/src
 # first-time index
 echo "** Running first-time indexing without vcs history"
 cd /opengrok/bin
-./OpenGrok -q -P -r off index
+OPENGROK_SCAN_REPOS=0 ./OpenGrok index
 
 # ... and we keep running the indexer to keep the container on
 echo "** Waiting for source updates..."
@@ -15,5 +15,5 @@ touch $OPENGROK_INSTANCE_BASE/reindex
 inotifywait -mr -e CLOSE_WRITE $OPENGROK_INSTANCE_BASE/src | while read f; do
   printf "*** %s\n" "$f"
   echo "*** Updating index without vcs history"
-  ./OpenGrok -q -P -r off index
+  OPENGROK_SCAN_REPOS=0 ./OpenGrok index
 done
